b6df35eff1d2df93042c13cd6581ddf15db09922
PATarghetta <patarghetta@gmail.com>
2021-10-18 16:00:41 -0300

    make it better

--- src/main/java/br/agendamedica/model/Usuario.java
+++ src/main/java/br/agendamedica/model/Usuario.java
@@ -5,49 +5,70 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.persistence.CollectionTable;
-import javax.persistence.Column;
 import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class Usuario {
+@Entity
+public class Usuario extends AbstractEntity{
 
-	@ElementCollection(fetch = FetchType.EAGER)
-	@CollectionTable(name = "tb_perfil")
-	private Set<Integer> perfis = new HashSet<>();
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
 
-	public Set<TipoPerfil> getPerfis() {
-		return perfis.stream().map(x -> TipoPerfil.toEnum(x)).collect(Collectors.toSet());
-	}
+    private String nome;
+    private String login;
+    private String senha;
 
-	public void addPerfil(TipoPerfil perfil) {
-		this.perfis.add(perfil.getCod());
-	}
 
-	@Column(name = "nm_login", length = 80, unique = true)
-	private String login;
 
-	@Column(name = "nm_senha")
-	private String senha;
+    public Usuario() {
+        // TODO Auto-generated constructor stub
+    }
 
-	public String getLogin() {
-		return login;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setLogin(String login) {
-		this.login = login;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	@JsonIgnore
-	public String getSenha() {
-		return senha;
-	}
+    public String getLogin() {
+        return login;
+    }
 
-	@JsonProperty
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
+    public void setLogin(String login) {
+        this.login = login;
+    }
 
+    @JsonIgnore
+    public String getSenha() {
+        return senha;
+    }
+
+    @JsonProperty
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setPerfis(Set<Integer> perfis) {
+        this.perfis = perfis;
+    }
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "tb_perfil")
+    private Set<Integer> perfis = new HashSet<>();
+
+    public Set<TipoPerfil> getPerfis() {
+        return perfis.stream().map(x -> TipoPerfil.toEnum(x)).collect(Collectors.toSet());
+    }
+
+    public void addPerfil(TipoPerfil perfil) {
+        this.perfis.add(perfil.getCod());
+    }
 }
